bd19855988f06c6f99fb18b0a65fc8d9c65838a7
Pavel Vazquez Faci <p.v.faci@medisin.uio.no>
2021-11-22 11:46:34 +0100

    changes in the clustering methodologies

--- labstract.py
+++ labstract.py
@@ -5,10 +5,10 @@
 @author: pavelvazquez
 """
 from sklearn.feature_extraction.text import TfidfVectorizer,CountVectorizer
-from sklearn.metrics.pairwise import cosine_similarity
+#from sklearn.metrics.pairwise import cosine_similarity
 #import igraph
 #import numpy as np
-import seaborn as sns
+#import seaborn as sns
 import matplotlib.pyplot as plt
 from scipy.sparse import csr_matrix
 from sklearn.decomposition import TruncatedSVD
@@ -21,15 +21,14 @@ parser = argparse.ArgumentParser()
 parser.add_argument('-i','--input', help= 'path to input file', default='./onchip_summary.csv')
 parser.add_argument('-o','--output', help= 'path to output file of abstracts', default='./data_summary.json')
 parser.add_argument('-t','--type', help= 'vectorizer type. Use: count or tfidf', default='tfidf')
-parser.add_argument('-n','--components', help= 'Number of components to use', default=10)
-parser.add_argument('-c','--cluster', help= 'Number of clusters to explore', default=3)
-parser.add_argument('-d','--distance', help="Optional. Calculate distance matrix and plot a heatmap")
-parser.add_argument('-m','--mode', help= 'Test or cluster', default='test')
+parser.add_argument('-n','--components', help= 'Number of components to use', default=5)
+parser.add_argument('-c','--cluster', help= 'Number of clusters to use', default=3)
+parser.add_argument('-m','--mode', help= 'Test, kcluster (K-Means Clustering) or acluster(Agglomerative Clustering *)')
+parser.add_argument('-a','--affinity', help= 'To be used allong with Agglomerative Clustering. Can be “euclidean”, “l1”, “l2”, “manhattan”, “cosine”, default is cosine', default='cosine')
 parser.add_argument('-v','--verbose', help="Optional. Plot 3d")
 args = parser.parse_args()
 
 
-##Variable parser
 
 def wm2df(wm, feat_names):
     
@@ -39,7 +38,52 @@ def wm2df(wm, feat_names):
                       columns=feat_names)
     return(df)
 
+###PCA componentsevaluation
+def test(variance,X):
+    fig, (ax1, ax2) = plt.subplots(2,1)
+    PC = range(1, tsvd.n_components+1)
+    ax1.bar(PC, variance, color='red')
+ 
+    
+    ##Kmeans inertias evaluation
+    from sklearn.cluster import KMeans 
+    inertias = []
+    for k in range(1,10):
+        model = KMeans(n_clusters=k) 
+        model.fit(X[:,:3])
+        inertias.append(model.inertia_)
+    ##Plot
+    ax2.plot(range(1,10), inertias, '-p', color='red')
+    ax1.set_title('PCA Components')
+    ax2.set_title('Clusters')
+    plt.show()
+    return()
 
+###Cluster by Kmeans
+def cluster(ncluster,X):
+    from sklearn.cluster import KMeans
+    model = KMeans(n_clusters=ncluster)
+    model.fit(X[:,:2])
+    labels = model.predict(X[:,:2])  
+
+    from mpl_toolkits.mplot3d import Axes3D
+    fig = plt.figure()
+    ax = Axes3D(fig)
+    ax.scatter(X[:,0],X[:,1],X[:,2], c=labels)
+    plt.show()
+    return()
+
+####Cosine similarity
+def cosinef(X,Xs,ncluster):
+    from sklearn.cluster import AgglomerativeClustering
+    model=AgglomerativeClustering(affinity='cosine',n_clusters=ncluster,linkage='complete').fit(X)
+    labels=model.labels_
+    from mpl_toolkits.mplot3d import Axes3D
+    fig = plt.figure()
+    ax = Axes3D(fig)
+    ax.scatter(Xs[:,0],Xs[:,1],Xs[:,2], c=labels)
+    plt.show()
+    return()
 
 
 ##Variable parser
@@ -95,43 +139,18 @@ X_sparse_tsvd = tsvd.fit(X_sparse).transform(X_sparse)
 
 if mode=='test':
     ###PCA componentsevaluation
-    fig, (ax1, ax2) = plt.subplots(2,1)
-    PC = range(1, tsvd.n_components+1)
-    ax1.bar(PC, tsvd.explained_variance_ratio_, color='red')
- 
-    
-    ##Kmeans inertias evaluation
-    from sklearn.cluster import KMeans 
-    inertias = []
-    for k in range(1,10):
-        model = KMeans(n_clusters=k) 
-        model.fit(X_sparse_tsvd[:,:3])
-        inertias.append(model.inertia_)
-    ##Plot
-    ax2.plot(range(1,10), inertias, '-p', color='red')
-    ax1.set_title('PCA Components')
-    ax2.set_title('Clusters')
-    plt.show()
+    test(tsvd.explained_variance_ratio_,X_sparse_tsvd)
 
-if mode=='cluster':
+
+if mode=='kcluster':
     #Cluster by Kmeans
-    from sklearn.cluster import KMeans
-    model = KMeans(n_clusters=ncluster)
-    model.fit(X_sparse_tsvd[:,:2])
-    labels = model.predict(X_sparse_tsvd[:,:2])  
-    #plt.scatter(X_sparse_tsvd[:,0], X_sparse_tsvd[:,1], c=labels)
-    #3D plot
-    from mpl_toolkits.mplot3d import Axes3D
-    fig = plt.figure()
-    ax = Axes3D(fig)
-    plot=ax.scatter(X_sparse_tsvd[:,0],X_sparse_tsvd[:,1],X_sparse_tsvd[:,2], c=labels)
-    plt.show()
+    cluster(ncluster,X_sparse_tsvd)
+
  
 #Calculate the distance matrix
-if args.distance:
-    G=cosine_similarity(X, X)
-    #Plot the heatmap
-    ax = sns.heatmap(G)
+if mode=="acluster":
+    cosinef(X,X_sparse_tsvd,ncluster)
+
 
 
 
